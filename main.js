9563dcea4cd7f2d10b543505fdb95b30e42face5
Daisuke-Matsubara <110824363+Daisuke-Matsubara@users.noreply.github.com>
2023-02-08 13:15:36 +0900

    JavaScript初級1 繰り返し処理課題

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-let number = 1;
-while (number <= 100){
+let number =1;
+while (number <= 100) {
+    if(number %3 === 0 && number %5 === 0){
+        console.log('FizzBuzz!!')
+    } else if(number %3 === 0){
+        console.log('Fizz!');
+    } else if(number %5 ===0){
+        console.log('Buzz!');
+    }  
     console.log(number);
     number++;
 }
